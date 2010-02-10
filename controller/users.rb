c9b498d7d8d93272629b10b25c212bd8619d80b8
Vincent Behar <v.behar@free.fr>
2010-02-10 11:19:10 +0100

    small markup fix

--- controller/users.rb
+++ controller/users.rb
@@ -1,11 +1,11 @@
 
 get '/groups' do
-  @list = Group.find(:all).collect{|g| g.name + "<br>"}
+  @list = Group.find(:all).collect{|g| g.name }
   erb :list
 end
 
 get '/users' do
-  @list = User.find(:all).collect{|u| u.name + "<br>"}
+  @list = User.find(:all).collect{|u| u.name }
   erb :list
 end
 
