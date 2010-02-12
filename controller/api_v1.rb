17aae5ca718d8e1e4d17b0eb730668dca7b5edf0
Vincent Behar <v.behar@free.fr>
2010-02-12 17:58:11 +0100

    better handling of extensions and content_types (not working)

--- controller/api_v1.rb
+++ controller/api_v1.rb
@@ -1,10 +1,13 @@
 
 before do
-  #case request.path_info.match /^\/api\/v1\/([a-z]{3,4})\//
-  if request.path_info.match /^\/api\/v1/
-    content_type 'application/xml', :charset => 'utf-8'
-  end
+  parse_extension request.path_info
 end
+
+# requires sinatra >= 1.0a
+#after do
+#  value = convert_return_value response.body
+#  response.body = value unless value.nil?
+#end
 
 get '/api/v1/users' do
   User.find(:all).to_xml
@@ -18,4 +21,21 @@ get '/api/v1/user/:uid' do |uid|
   User.find(uid).to_xml
 end
 
+def parse_extension(request_path)
+  ext_pattern = /^\/api\/v1\/.*\.([a-z]{3,4})$/
+  @ext = request_path.match(ext_pattern)[1] rescue nil
+  @content_type = case @ext
+    when 'xml'; 'application/xml'
+    when 'json'; 'application/json'
+    when 'yaml'; 'text/yaml'
+    else @ext = nil; nil
+  end
+  content_type @content_type, :charset => 'utf-8' if @content_type
+end
+
+def convert_return_value(value)
+  m = "to_#{@ext}".to_sym
+  return value.send(m) if value.respond_to? m
+  return nil
+end
 
