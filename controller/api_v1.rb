72932af669d8f0e81496fa31118a477c974ccad0
Vincent Behar <v.behar@free.fr>
2010-02-12 20:48:05 +0100

    cleanup api

--- controller/api_v1.rb
+++ controller/api_v1.rb
@@ -1,29 +1,8 @@
 
 before do
-  parse_extension request.path_info
-end
-
-# requires sinatra >= 1.0a
-#after do
-#  value = convert_return_value response.body
-#  response.body = value unless value.nil?
-#end
-
-get '/api/v1/users' do
-  User.find(:all).to_xml
-end
-
-get '/api/v1/users/search/:attr/:value' do |attr, value|
-  User.find(:all, :attribute => attr, :value => value).to_xml
-end
-
-get '/api/v1/user/:uid' do |uid|
-  User.find(uid).to_xml
-end
-
-def parse_extension(request_path)
-  ext_pattern = /^\/api\/v1\/.*\.([a-z]{3,4})$/
-  @ext = request_path.match(ext_pattern)[1] rescue nil
+  ext_pattern = /^(\/api\/v1\/.*)\.([a-z]{3,4})$/
+  match = request.path_info.match(ext_pattern)
+  @ext = match[2] rescue nil
   @content_type = case @ext
     when 'xml'; 'application/xml'
     when 'json'; 'application/json'
@@ -31,11 +10,23 @@ def parse_extension(request_path)
     else @ext = nil; nil
   end
   content_type @content_type, :charset => 'utf-8' if @content_type
+  request.path_info = match[1] unless @ext.nil?
+end
+
+get '/api/v1/users' do
+  convert_return_value User.find(:all)
+end
+
+get '/api/v1/users/:attr/:value' do |attr, value|
+  convert_return_value User.find(:all, :attribute => attr, :value => value)
+end
+
+get '/api/v1/user/:uid' do |uid|
+  convert_return_value User.find(uid)
 end
 
 def convert_return_value(value)
   m = "to_#{@ext}".to_sym
-  return value.send(m) if value.respond_to? m
-  return nil
+  value.respond_to?(m) ? value.send(m) : value.to_s rescue ""
 end
 
