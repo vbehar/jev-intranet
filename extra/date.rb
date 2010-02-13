b46a59e8d2e9a18eed5ed431cbc0bd56fd24a2d0
Vincent Behar <v.behar@free.fr>
2010-02-13 13:15:12 +0100

    fix 'already initialized constant' for date names

--- extra/date.rb
+++ extra/date.rb
@@ -3,17 +3,17 @@
 class Date
   alias :strftime_nolocale :strftime
 
-  ABBR_DAYNAMES = %w( Dim Lun Mar Mer Jeu Ven Sam )
-  DAYNAMES = %w( Dimanche Lundi Mardi Mercredi Jeudi Vendredi Samedi )
-  ABBR_MONTHNAMES = [nil] + %w( Jan Fév Mars Avr Mai Juin Juil Août Sept Oct Nov Déc )
-  MONTHNAMES = [nil] + %w( Janvier Février Mars Avril Mai Juin Juillet Août Septembre Octobre Novembre Décembre )
+  ABBR_DAYNAMES_FR = %w( Dim Lun Mar Mer Jeu Ven Sam )
+  DAYNAMES_FR = %w( Dimanche Lundi Mardi Mercredi Jeudi Vendredi Samedi )
+  ABBR_MONTHNAMES_FR = [nil] + %w( Jan Fév Mars Avr Mai Juin Juil Août Sept Oct Nov Déc )
+  MONTHNAMES_FR = [nil] + %w( Janvier Février Mars Avril Mai Juin Juillet Août Septembre Octobre Novembre Décembre )
 
   def strftime(format)
     format = format.dup
-    format.gsub!(/%a/, ABBR_DAYNAMES[self.wday])
-    format.gsub!(/%A/, DAYNAMES[self.wday])
-    format.gsub!(/%b/, ABBR_MONTHNAMES[self.mon])
-    format.gsub!(/%B/, MONTHNAMES[self.mon])
+    format.gsub!(/%a/, ABBR_DAYNAMES_FR[self.wday])
+    format.gsub!(/%A/, DAYNAMES_FR[self.wday])
+    format.gsub!(/%b/, ABBR_MONTHNAMES_FR[self.mon])
+    format.gsub!(/%B/, MONTHNAMES_FR[self.mon])
     self.strftime_nolocale(format)
   end
 end
