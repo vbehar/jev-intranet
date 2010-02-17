c3b47d0bc6660d7adb2df93b0c9803b07f089036
Vincent Behar <v.behar@free.fr>
2010-02-17 20:30:27 +0100

    integrate gravatar

--- controller/helper.rb
+++ controller/helper.rb
@@ -1,5 +1,19 @@
+require 'md5'
 
 helpers do 
+
+  # return the gravatar url for the current user
+  def gravatar()
+    gravatar_for(@me.mail(true).first)
+  end
+
+  # return the gravatar url for the given mail address
+  def gravatar_for(mail)
+    encoded_mail = MD5::md5(mail.downcase) rescue nil
+    "http://www.gravatar.com/avatar/#{encoded_mail}?s=80"
+  end
+
+  # ADMIN HELPERS
 
   # retrieve the uid of the previous user (based on the list of users present in session)
   def prev_user(current_user = nil)
