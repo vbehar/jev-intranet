f863bcea1397a1a78bd0887bc4f6e21a0e3106f6
Vincent Behar <v.behar@free.fr>
2010-02-20 16:58:49 +0100

    basic 'birthdays' page

--- controller/users.rb
+++ controller/users.rb
@@ -1,4 +1,12 @@
 
+get '/birthdays' do
+  @current_month = Date.today
+  @next_month = @current_month.next_month
+  @current_birthdays = users_birthdays_for @current_month
+  @next_birthdays = users_birthdays_for @next_month
+  erb :birthdays
+end
+
 get '/users' do
   @users = User.find(:all, :attributes => [:uid, :cn, :sn, :gn, :displayName, :title, :ffckCategory])
   erb :users
@@ -16,5 +24,10 @@ end
 post '/account' do
   puts params['user'].inspect
   redirect '/account'
+end
+
+# return an array of users born in the given month
+def users_birthdays_for(date)
+  User.find(:all, :attribute => 'birthDate', :value => date.strftime("*-%m-*"), :attributes => [:uid, :cn, :displayName, :birthDate]).sort{|a,b| a.birth_date.day <=> b.birth_date.day}
 end
 
