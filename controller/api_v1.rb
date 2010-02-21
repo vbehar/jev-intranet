8bbeb634d97f845f6eefe2f8a84a055a2170fce1
Vincent Behar <v.behar@free.fr>
2010-02-21 13:04:38 +0100

    do not cache api responses

--- controller/api_v1.rb
+++ controller/api_v1.rb
@@ -1,16 +1,22 @@
 
 before do
-  ext_pattern = /^(\/api\/v1\/.*)\.([a-z]{3,4})$/
-  match = request.path_info.match(ext_pattern)
-  @ext = match[2] rescue nil
-  @content_type = case @ext
-    when 'xml'; 'application/xml'
-    when 'json'; 'application/json'
-    when 'yaml'; 'text/yaml'
-    else @ext = nil; nil
+  if request.path_info.match(/^\/api\/v1/)
+    # do not cache
+    expires 0, :private, :no_cache, :no_store
+
+    # auto-set content_type, based on the extension
+    ext_pattern = /^(\/api\/v1\/.*)\.([a-z]{3,4})$/
+    match = request.path_info.match(ext_pattern)
+    @ext = match[2] rescue nil
+    @content_type = case @ext
+      when 'xml'; 'application/xml'
+      when 'json'; 'application/json'
+      when 'yaml'; 'text/yaml'
+      else @ext = nil; nil
+    end
+    content_type @content_type, :charset => 'utf-8' if @content_type
+    request.path_info = match[1] unless @ext.nil?
   end
-  content_type @content_type, :charset => 'utf-8' if @content_type
-  request.path_info = match[1] unless @ext.nil?
 end
 
 get '/api/v1/users' do
