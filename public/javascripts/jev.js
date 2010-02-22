f916e7b66ef772b0a6e8facc7a6c67c7801e6a05
Vincent Behar <v.behar@free.fr>
2010-02-22 23:12:01 +0100

    user account form : start working on text fields duplication/remove

--- public/javascripts/jev.js
+++ public/javascripts/jev.js
@@ -2,6 +2,16 @@ if(typeof Intranet == "undefined") {
   var Intranet = {};
 }
 
+// work in progress
+Intranet.form = function() {
+  var orig = $('#mydiv');
+  var dup = orig.clone();
+  var html = dup.html();
+  html = html.replace(/my_id/g, "new id");
+  dup.html(html);
+  dup.insertAfter(orig);
+}
+
 $(document).ready(function() {
   $.getJSON('/account.json', function(user) {
     Intranet.current_user = user;
