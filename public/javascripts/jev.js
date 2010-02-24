d9cdcea2bb530041da0290a8f1e0381892e9673d
Vincent Behar <v.behar@free.fr>
2010-02-24 23:34:54 +0100

    account form: input field duplicate/removal with jquery is working (for email)

--- public/javascripts/jev.js
+++ public/javascripts/jev.js
@@ -2,13 +2,18 @@ if(typeof Intranet == "undefined") {
   var Intranet = {};
 }
 
-// work in progress
-Intranet.form = function() {
-  var orig = $('#mydiv');
+// Duplicate the given input form, remove its value,
+// and insert it after the original
+Intranet.dupInputForm = function(elem) {
+  var orig = elem;
   var dup = orig.clone();
+
+  // fix its html value
   var html = dup.html();
-  html = html.replace(/my_id/g, "new id");
+  html = html.replace(/value="(.*)"/, "value=\"\"");
+  html = html.replace(/(\d+)/g, function(m,n) { return Number(n) + 1; });
   dup.html(html);
+
   dup.insertAfter(orig);
 }
 
