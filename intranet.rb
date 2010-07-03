8c57bf112b020adbb0e7e549dcf780e8ad20c679
Vincent Behar <v.behar@free.fr>
2010-07-03 12:44:15 +0200

    cleanup 'require'

--- intranet.rb
+++ intranet.rb
@@ -3,17 +3,17 @@
 
 # extra, aka monkey patches
 %w(string date).each do |f|
-  require File.dirname(__FILE__) + "/extra/#{f}"
+  require(File.join(File.dirname(__FILE__), 'extra', f))
 end
 
 # domain classes
 %w(user group post).each do |f|
-  require File.dirname(__FILE__) + "/domain/#{f}"
+  require(File.join(File.dirname(__FILE__), 'domain', f))
 end
 
 # controllers (sinatra routes)
 %w(helper main account users admin api_v1).each do |f|
-  require File.dirname(__FILE__) + "/controller/#{f}"
+  require(File.join(File.dirname(__FILE__), 'controller', f))
 end
 
 # initialization
