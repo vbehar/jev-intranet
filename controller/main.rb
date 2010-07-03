c145ad53e655c189d5c07f1133657c4e1e639b82
Vincent Behar <v.behar@free.fr>
2010-07-04 00:32:11 +0200

    fix use of url parameters (for rack-cache)

--- controller/main.rb
+++ controller/main.rb
@@ -11,17 +11,26 @@ before do
   expires 10.minutes, :public
 end
 
-get '/' do
-  @page = params[:page].to_i
+['/', '/posts', '/posts/'].each do |path|
+  get path do
+    display_posts(params[:page])
+  end
+end
+get '/posts/:page' do |page|
+  display_posts(page)
+end
+
+get '/about' do
+  erb :about
+end
+
+def display_posts(page)
+  @page = page.to_i
   @page = 1 unless @page.is_a?(Fixnum) && @page > 0
   posts_count = Post.count
   @pages = posts_count / POSTS_PER_PAGE
   @pages += 1 if (posts_count % POSTS_PER_PAGE) > 0
   @posts = Post.sort(:created_at.desc).limit(POSTS_PER_PAGE).skip((@page-1)*POSTS_PER_PAGE).all
   erb :index
-end
-
-get '/about' do
-  erb :about
 end
 
