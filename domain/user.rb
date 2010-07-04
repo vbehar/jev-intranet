490950149a6f7009c946ed9445740f60332c8c09
Vincent Behar <v.behar@free.fr>
2010-07-04 20:31:00 +0200

    add method for getting all posts that belongs to a user

--- domain/user.rb
+++ domain/user.rb
@@ -62,5 +62,10 @@ class User < LdapBase
   def male?; gender == 'M'; end
   def female?; gender == 'F'; end
 
+  # return all posts that belongs to the user
+  def posts
+    Post.where(:user_id => user_id).all
+  end
+
 end
 
