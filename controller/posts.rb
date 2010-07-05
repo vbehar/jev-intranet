cc6590fe1aa346eb2cd9ec906be89eb341706ac0
Vincent Behar <v.behar@free.fr>
2010-07-05 20:11:22 +0200

    new feature : display all posts or only posts from a given user (or current user)

--- controller/posts.rb
+++ controller/posts.rb
@@ -1,8 +1,15 @@
 
-['/posts', '/posts/', '/posts/:page'].each do |path|
+['/posts', '/posts/', '/posts/:user', '/posts/:user/', '/posts/:user/:page'].each do |path|
   get path do
-    expires 1.minutes, :public
-    load_posts(params[:page])
+    if params[:user].nil? || params[:user].eql?('all')
+      expires 1.minutes, :public
+      @user = 'all'
+      load_posts(params[:page])
+    else
+      expires 0, :private, :no_cache, :no_store
+      @user = ( params[:user].eql?('mine') ? current_user_id : params[:user] )
+      load_posts(params[:page], :user_id => @user)
+    end
     erb :posts
   end
 end
@@ -15,13 +22,13 @@ post '/posts' do
   redirect '/posts'
 end
 
-def load_posts(page = 1)
+def load_posts(page = 1, query_params = {})
   @page = page.to_i
   @page = 1 unless @page.is_a?(Fixnum) && @page > 0
-  posts_count = Post.count
+  posts_count = Post.where(query_params.merge(:deleted.ne => true)).count
   @pages = posts_count / options.posts_per_page
   @pages += 1 if (posts_count % options.posts_per_page) > 0
-  @posts = Post.paginate(:per_page => options.posts_per_page, :page => @page,
-                         :deleted.ne => true, :order => :created_at.desc)
+  @posts = Post.paginate(query_params.merge(:per_page => options.posts_per_page, :page => @page,
+                                            :deleted.ne => true, :order => :created_at.desc))
 end
 
