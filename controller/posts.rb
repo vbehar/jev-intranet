3e30a730713a36dc791778ccf69a516f0f601504
Vincent Behar <v.behar@free.fr>
2010-08-01 18:03:55 +0200

    when displaying posts for a given user, also display the name of that user

--- controller/posts.rb
+++ controller/posts.rb
@@ -5,11 +5,13 @@
     if params[:user].blank? || params[:user].eql?('all')
       expires 1.minutes, :public
       @user = 'all'
+      @user.instance_eval{ def uid(); self; end } # fake uid for constructing urls
       load_posts(params[:page])
     else
       expires 0, :private, :no_cache, :no_store
-      @user = params[:user]
-      load_posts(params[:page], :user_id => @user)
+      @user = User.find(params[:user]) rescue nil
+      pass if @user.nil?
+      load_posts(params[:page], :user_id => @user.uid)
     end
     erb :posts
   end
