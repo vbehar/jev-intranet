d7d22eccc25eac262b919b487fb813409a2f0e90
Vincent Behar <v.behar@free.fr>
2010-08-17 22:57:30 +0200

    new rake task used to initialize subscriptions for 2010

---
+++ scripts/rake/init_users_subscriptions.rb
@@ -0,0 +1,20 @@
+# rake tasks for initializing the subscriptions for users
+
+namespace :users do
+
+  desc "Initialize the subscriptions for 2010"
+  task :init_subscriptions do
+    User.all.each do |u|
+      s = Subscription.new
+      s.user_id = u.uid
+      s.year = 2010
+      s.medical_certificate_type = u.medical_certificate_type
+      s.medical_certificate_date = u.medical_certificate_date
+      s.created_at = u.ffck_number_date
+      s.state = 'sent'
+      s.save unless s.created_at.nil?
+    end
+  end
+
+end
+
