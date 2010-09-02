1a89268b64ff24b4cf334cccb8e248d0df18a098
Vincent Behar <v.behar@free.fr>
2010-09-03 01:06:20 +0200

    only init subscription if it does not already exist

--- scripts/rake/init_users_subscriptions.rb
+++ scripts/rake/init_users_subscriptions.rb
@@ -5,14 +5,16 @@ namespace :users do
   desc "Initialize the subscriptions for 2010"
   task :init_subscriptions do
     User.all.each do |u|
-      s = Subscription.new
-      s.user_id = u.uid
-      s.year = 2010
-      s.medical_certificate_type = u.medical_certificate_type
-      s.medical_certificate_date = u.medical_certificate_date
-      s.created_at = u.ffck_number_date
-      s.state = 'sent'
-      s.save unless s.created_at.nil?
+      unless Subscription.where(:user_id => u.uid, :year => 2010).exist?
+        s = Subscription.new
+        s.user_id = u.uid
+        s.year = 2010
+        s.medical_certificate_type = u.medical_certificate_type
+        s.medical_certificate_date = u.medical_certificate_date
+        s.created_at = u.ffck_number_date
+        s.state = 'sent'
+        s.save unless s.created_at.nil?
+      end
     end
   end
 
